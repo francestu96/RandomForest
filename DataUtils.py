fd4fd0ccffe58d65a234070044455e68dbae1cc7
Francesco Stucci <francesco.stucci@horsaspa.onmicrosoft.com>
2020-01-29 10:47:30 +0100

    clean

--- DataUtils.py
+++ DataUtils.py
@@ -4,37 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-def top_accident_by_state(dataset):
-    fig,ax=plt.subplots(1,2,figsize=(15,8))
-    clr = ("blue", "forestgreen", "gold", "red", "purple",'cadetblue','hotpink','orange','darksalmon','brown')
-    dataset.State.value_counts().sort_values(ascending=False)[:10].sort_values().plot(kind='barh',color=clr,ax=ax[0])
-    ax[0].set_title("Top 10 Acciedent Prone States",size=20)
-    ax[0].set_xlabel('States',size=18)
-
-
-    count=dataset['State'].value_counts()
-    groups=list(dataset['State'].value_counts().index)[:10]
-    counts=list(count[:10])
-    counts.append(count.agg(sum)-count[:10].agg('sum'))
-    groups.append('Other')
-    type_dict=pd.DataFrame({"group":groups,"counts":counts})
-    clr1=('brown','darksalmon','orange','hotpink','cadetblue','purple','red','gold','forestgreen','blue','plum')
-    qx = type_dict.plot(kind='pie', y='counts', labels=groups,colors=clr1,autopct='%1.1f%%', pctdistance=0.9, radius=1.2,ax=ax[1])
-    plt.legend(loc=0, bbox_to_anchor=(1.15,0.4)) 
-    plt.subplots_adjust(wspace =0.5, hspace =0)
-    plt.ylabel('')
-    return plt
-
-def setUpUSAccidentDataset(features, dataset):
-    features.remove('Start_Time')
-    features.append('Hour')
-    dataset = dataset.sample(100000)
-    dataset['Hour']=pd.to_datetime(dataset['Start_Time'], errors='coerce').dt.hour
-    dataset.drop("Start_Time", axis=1, inplace=True)
-    dataset = dataset.dropna()
-    dataset.drop(dataset[(dataset['State'] == 'CA') | (dataset['State'] == 'TX')].index, inplace=True)
-    return dataset
-
 def setUpWineDataset(dataset):
     dataset = dataset.sample(10000)
     dataset.drop_duplicates('description', inplace=True)
--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # RandomForest
 Python RandomForest implementation from scratch.
 
-There are two classifiers for two differents dataset retrived on Kuggle.
+In this repo there is a classifier for WineReviews dataset retrived on Kuggle (https://www.kaggle.com/zynicide/wine-reviews)
 
-In this repo there is just WineReviews.csv (https://www.kaggle.com/zynicide/wine-reviews)
+The goal of this classifier is to to classify correctly the wine variety based on wine reviews. 
 
-The US_Accident dataset is too big to be uploaded (https://www.kaggle.com/sobhanmoosavi/us-accidents)
+I've found the best model which fits better this datasets is the Random Forest one.
 
-In the first dataset I try to classify wine variety whereas in the second one the US country where the accident appened.
-In both cases, I've found the best model which fits better our datasets is the Random Forest one.
-
-This Random Forest in implemented by a bagging of Decision Tree models. Each tree is a thread in order to speed up the computation time.
+This Random Forest is implemented by a bagging of Decision Tree models. Each tree is a thread in order to speed up the computation time.
 There is also a "Tree" folder where there is stored a file for each tree. There it's shown the tree stucture and the questions each node corresponds to.
